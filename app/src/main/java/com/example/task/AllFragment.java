934ee02f07700544f35b8fa926070d5cadb49ed5
hosseinkasiri <hosseinkasiri5@gmail.com>
2020-05-30 14:54:10 +0430

    create add button

--- app/src/main/java/com/example/task/AllFragment.java
+++ app/src/main/java/com/example/task/AllFragment.java
@@ -22,7 +22,9 @@ public class AllFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_all, container, false);
+
+        return view;
     }
 }
