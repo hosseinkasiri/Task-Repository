ccd6c236797dca8162066a984975975c3b676841
hosseinkasiri <hosseinkasiri5@gmail.com>
2020-05-30 13:29:56 +0430

    Create class ViewPagerAdapter

---
+++ app/src/main/java/com/example/task/ViewPagerAdapter.java
@@ -0,0 +1,50 @@
+package com.example.task;
+
+import android.widget.LinearLayout;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentStatePagerAdapter {
+
+    private final List<Fragment> mFragments = new ArrayList<>();
+    private final List<String> mFragmentTitle = new ArrayList<>();
+
+
+    public ViewPagerAdapter(@NonNull FragmentManager fm) {
+        super(fm);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return mFragments.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentTitle.size();
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return mFragmentTitle.get(position);
+    }
+
+    public void addFragment(Fragment fragment , String title){
+
+        mFragments.add(fragment);
+        mFragmentTitle.add(title);
+
+
+    }
+
+
+}
